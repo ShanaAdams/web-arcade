af8212d5beaf8fd82be32f9b68e9be0fc617471b
Shana Adams <shana.adams10@gmail.com>
2024-04-27 18:44:06 -0600

    Create canvas with a black border.

---
+++ snake.js
@@ -0,0 +1,7 @@
+let canvas = document.getElementById("gameCanvas");
+let context = canvas.getContext("2d");
+
+context.fillStyle = "white";
+context.fillRect(0, 0, 300, 300);
+context.strokeStyle = "black";
+context.strokeRect(0, 0, 300, 300);
