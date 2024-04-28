7933879e9ec8edd8d0e865cf7ff6462582f3d2a1
Shana Adams <shana.adams10@gmail.com>
2024-04-28 17:46:57 -0600

    refactor code that draws the canvas into a clearCanvas function.

--- snake.js
+++ snake.js
@@ -1,10 +1,11 @@
 const canvas = document.getElementById("gameCanvas");
 const context = canvas.getContext("2d");
-
-context.fillStyle = "white";
-context.fillRect(0, 0, 300, 300);
-context.strokeStyle = "black";
-context.strokeRect(0, 0, 300, 300);
+function clearCanvas() {
+  context.fillStyle = "white";
+  context.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+  context.strokeStyle = "black";
+  context.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+}
 
 let snake = [
   { x: 150, y: 150 },
@@ -13,6 +14,8 @@ let snake = [
   { x: 120, y: 150 },
   { x: 110, y: 150 },
 ];
+let dx = 10;
+let dy = 0;
 
 function drawSnakePart(snakePart) {
   context.fillStyle = "lightgreen";
@@ -25,13 +28,22 @@ function drawSnake() {
   snake.forEach(drawSnakePart);
 }
 
-drawSnake();
-
-let dx = 10;
-let dy = 0;
+/*
+ * Advances the snake by changing the x-coordinates of its parts
+ * according to the horizontal velocity and the y-coordinates of its parts
+ * according to the vertical veolocity
+ */
 
 function advanceSnake() {
   const head = { x: snake[0].x + dx, y: snake[0].y + dy };
   snake.unshift(head);
   snake.pop();
 }
+
+clearCanvas();
+advanceSnake();
+advanceSnake();
+advanceSnake();
+advanceSnake();
+advanceSnake();
+drawSnake();
